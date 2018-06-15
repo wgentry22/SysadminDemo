5d6b9ca569fa26a45c5a4439cc9a956583d10a2e
wtg1890 <william.gentry02@gmail.com>
2018-06-15 16:15:35 -0400

    this build will fail

---
+++ src/test/java/com/revature/NewTest.java
@@ -0,0 +1,12 @@
+package com.revature;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  assertTrue(false);
+  }
+}
